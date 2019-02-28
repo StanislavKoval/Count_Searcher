ebed5e6853a90d52d8741dc985e632708b4901b5
Koval Stanislav <staskoval7@gmail.com>
2019-02-28 14:42:31 +0300

    v.1.0 28/02

---
+++ Main.py
@@ -0,0 +1,329 @@
+import pandas as pd
+import numpy as np
+import re
+import pymorphy2
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from tqdm import tqdm
+'''
+Notes:
+1.не таблицу выводить, а только номера без повторов по слову или фразе
+2. сделать в препроцессор запись в файл
+
+
+def row_preprocessor(str):
+
+    abbreviation = ["рад", "мрад", "мкрад",  # угол
+                    "см", "м", "мм", "мкм", "нм", "дм",  # метр
+                    "кг", "мг", "мкг",  # вес
+                    "мин", "ч", "сут", "с", "мс", "мкс", "нс",  # время
+                    "л",  # объем
+                    "Гц", "ГГц", "МГц", "кГц",  # ГЦ
+                    "шт", # кол-во
+                    "ом","а","в"]# эл-тех
+
+    str = re.sub(r'[]«[}{»()<>/+*_"-:;.,]', ' ', str).lower()
+    str = re.sub(r'[@#$%^&`|!?~]', ' ', str)
+    str = re.sub(r'\d+', '', str)
+    str = re.sub(r'\d+[xX]\d+| \d+[xX]\d+[xX]\d+', '', str)  # eng
+    str = re.sub(r'\d+[хХ]\d+| \d+[хХ]\d+[хХ]\d+', '', str)  # rus
+
+    str = re.sub("[^\w]", " ", str).split()
+
+    stop_words_rus = set(stopwords.words('russian'))
+    stop_words_eng = set(stopwords.words('english'))
+
+    words = [word for word in str   if not word in stop_words_rus]
+    words = [word for word in words if not word in stop_words_eng]
+    words = [word for word in words if not word in abbreviation]
+    str = ' '.join(words)
+
+    return str
+
+def reader(file):
+    file_str = file['FullName']  # данные
+    str1 = []
+    for row in file_str:
+        row = row_preprocessor(row)  # удаление пробелов
+        str1.append(row)
+    # print(str1)
+
+    file_count = file['Count']  # данные
+    str2 = []
+    for row in file_count:
+        str2.append(str(row))
+    # print(str2)
+
+    return str1, str2
+
+
+def select_rows(df, search_strings):
+    unq, IDs = np.unique(df, return_inverse=True)  # находит ункальные элементы в датасете
+    unqIDs = np.searchsorted(unq, search_strings)  # находит, сортирует по search strings
+    return df[((IDs.reshape(df.shape) == unqIDs[:, None, None]).any(-1)).all(0)]
+
+file = pd.read_csv('data/123.csv', sep='\t', encoding='PT154')
+
+str1, str2 = reader(file)
+
+df = pd.DataFrame({"Counts": str2, "Data": str1})
+#print(df)
+#print(df.loc[df["Data"].isin(["принтер brother ty"])])
+
+#df_modified = select_rows(df,["принтер"])
+#print(df_modified)
+'''
+
+test_str = "мама папа мама папа 23 принтер сканер"
+search_word = "23 "
+if search_word in test_str:
+    print(test_str)
+else:
+    print(None)
+
+
+
+
+
+
+'''
+#сделать счета строкой
+
+data = pd.DataFrame({"A":[" dd app app","ban", "babcy app"], "C":str2 })
+print(data)
+
+
+df_modified = select_rows(data,["ap"])
+print(df_modified)
+
+
+
+
+dict_out = {}
+for x, y in zip(str2, str1):
+    dict_out[x] = y
+print(dict_out)
+
+
+name1 = "мосгостранс"
+flag = True
+
+for search_emploers in dict_out:
+    if dict_out[search_emploers]['name'] == name1:
+        print(employer[search_emploers]['Telefon'])
+        flag = False
+
+if flag:
+    print('нет такого значения')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+lookup_list = pd.Series(dict_out)
+
+print(lookup_list)
+#print(lookup_list[lookup_list.values == 10.01])
+
+answer = lookup_list[lookup_list.values == 10.01].index
+print(answer)
+answer = pd.Index.tolist(answer)
+print(answer)
+
+
+
+
+
+
+
+a=[]
+def searcher (clue_word, dict_out):
+
+    if clue_word in dict_out.values():
+        a.append(dict_out.keys())
+    return print(a)
+
+#нужно написать
+
+a = searcher(clue_word, dict_out)
+#вход строка, вывод номер
+#print(list(dict_out.keys())[list(dict_out.values()).index("cредство чистящее универсальное  пемолюкс  гр  ")])
+
+
+
+for row in file:
+    #row = preprocessor(row)
+    str1.append(row)
+
+print(str1)
+print(len(str1))
+
+
+str2 = []
+file_count = file['Count']#счета
+for row in file_count:
+    str2.append(row)
+
+print(str2)
+print(len(str2))
+
+uniq_and_fifa = dict(zip(str2, str1))
+#print(uniq_and_fifa)
+
+
+dict_out = {}
+
+#for x, y in zip(str2, str1):
+    #dict_out[x] = y
+    #dict_out.update(y)
+#print(dict_out)
+
+
+
+file = pd.read_csv('data/123.csv', sep='\t', encoding='PT154')
+
+
+str1 = []
+file_str = file['FullName']#данные
+for row in file_str:
+    #row = preprocessor(row)
+    str1.append(row)
+    #вставим в словарь (value)
+
+print(str1)
+#print(type(str1))
+
+str2 = []
+file_count = file['Count']#счета
+for row in file_count:
+    str2.append(row)
+    #вствим в словарь (key)
+
+print(str2)
+#print(type(str2))
+
+
+pd.DataFrame(str2, columns=["BB", "aa"])    
+
+
+#voc = dict(zip(str2, str1))
+#print(voc)
+#print(type(voc))
+
+#bb=pd.Series(str2, str1)
+#print(bb)
+
+#for i in  str2.count():
+
+word = "поездка"
+
+if word in str2:
+    print(ffff)
+    print(str1)
+
+
+answer = finder(word,str)
+
+
+def finder (word, str):
+
+    answer = [print(str[i]) for i in range(len(str)) if word in str[i]]
+
+    return answer
+
+берем str
+очистка
+бьем на words
+очистка
+соединяем в str
+
+
+bb(str1)
+
+def bb (str):
+    words = [str]
+    words = [re.sub(r'\d+', '', str)]
+    #tokenizer = RegexpTokenizer('\w+-\w+|\w+')
+
+    #str = re.sub(r'\d+', '', str)
+    #words = [str]
+    return words
+    
+    for word in tokenizer.tokenize(str_list):
+        if (len(word) > 1):  # words longer 1 letters
+            word = re.sub(r'"', '', word)  # delete brackets in bigram words
+            word = morph.parse(word)[0].normal_form  # turn into a normal form
+            words.append(word.lower())  # making all letters lowercase
+
+        # making stop words free (rus and eng)
+    stop_words_rus = set(stopwords.words('russian'))
+    stop_words_eng = set(stopwords.words('english'))
+    words = [word for word in words if not word in stop_words_rus | stop_words_eng]
+
+
+
+
+for row in data_file:
+    str1.append(row)
+
+
+
+#str1.append(data_file)
+print(str1)
+
+def preprocessor(data_file):
+    # making string type
+    for column in data_file.columns:
+        data_file[column] = data_file[column].astype('str')  # строчки файла data_file имеют тип String
+
+    # token (only: word, word-word or bigrams)
+    tokenizer = RegexpTokenizer('\w+-\w+|"\w+\s\w+"|\w+')  # for word and word-word and "word word" (as bigram)
+
+    morph = pymorphy2.MorphAnalyzer()
+    words = list()  # list('abc') -> ['a', 'b','c']
+
+    # creat a list of "pure" normalised words from data_file
+    # working with columns, clean from brackets, signs and numbers
+    for i in tqdm(range(data_file.shape[0])):
+        for column in data_file.columns:
+            # убираем ковычки, скобки и знак запятой
+            data_file[column][i] = re.sub(r'[]«[}{»(,)<>]', ' ', data_file[column][i])
+            # удалим случаи типо 50х50 или 50х50х50
+            data_file[column][i] = re.sub(r'\d+[xX]\d+| \d+[xX]\d+[xX]\d+', ' ', data_file[column][i])  # eng
+            data_file[column][i] = re.sub(r'\d+[хХ]\d+| \d+[хХ]\d+[хХ]\d+', ' ', data_file[column][i])  # rus
+            # убираем знаки-разделители, заменяем пробелом
+            data_file[column][i] = re.sub(r'[/+*_:.]', ' ', data_file[column][i])
+            # убираем все цифры
+            data_file[column][i] = re.sub(r'\d+', '', data_file[column][i])
+            # убираем все знаки кроме "-"
+            data_file[column][i] = re.sub(r'[^a-zA-Z],[-]', '', data_file[column][i])
+
+            # working with words
+            # берет строки таблицы и делит их на составляющие (пока что он съедает тире и точки)
+            for word in tokenizer.tokenize(data_file[column][i]):
+                if (len(word) > 1):  # words longer 1 letters
+                    word = re.sub(r'"', '', word)  # delete brackets in bigram words
+                    word = morph.parse(word)[0].normal_form  # turn into a normal form
+                    words.append(word.lower())  # making all letters lowercase
+
+    # making stop words free (rus and eng)
+    stop_words_rus = set(stopwords.words('russian'))
+    stop_words_eng = set(stopwords.words('english'))
+    words = [word for word in words if not word in stop_words_rus | stop_words_eng]
+
+    # result
+    #   print(words)
+    #   print('Length of list "words"  with repeats: ', len(words))
+    #   print('Length of list "words"  without repeats: ', len(set(words)), "\n")
+    return words
+
+'''
