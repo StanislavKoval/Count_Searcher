9e8b5bed282b3b89af6cd108f74b974b94a94efa
Stanislav <34125332+StanislavKoval@users.noreply.github.com>
2019-04-25 17:21:36 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,42 +1,11 @@
-'''
-from nltk.collocations import *
-from tqdm import tqdm
-from nltk.stem import *
-import numpy as np
-from pymorphy2 import MorphAnalyzer, units
-import codecs
-import distance
-import textdistance
-from difflib import SequenceMatcher
-import re
-import pymorphy2
-from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
-from tqdm import tqdm
-import pymorphy2
-import numpy as np
-import pymorphy2
-import nltk
-import nltk.tokenize
-from nltk.corpus import stopwords
-from nltk.tokenize import RegexpTokenizer
-from nltk.tokenize import WordPunctTokenizer
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import WordPunctTokenizer
-from nltk.collocations import BigramCollocationFinder
-from nltk.metrics import BigramAssocMeasures
-#from Levenshtein import *
-#from jellyfish import *
-'''
 import pandas as pd
-import re
-from nltk.corpus import stopwords
+import numpy as np
 import difflib
 import pymorphy2
-import numpy as np
+import re
+
+from nltk.corpus import stopwords
 from tqdm import tqdm
-from tabulate import tabulate
 
 """
 Описание поставленной задачи 
@@ -67,7 +36,6 @@ from tabulate import tabulate
 
 На выход поступает nomenklatura_new.csv и файл с "UNK"
 
-//////////*** как варик написать метод def file_creator(name,place)
 """
 
 
@@ -131,22 +99,22 @@ def reader(file):
     return str1, str2
 
 
-def count_finder(file, SEARCH_WORD,column_name):
+def count_finder(file, SEARCH_WORD, column_name):
     str3 = []
     file = file[file[column_name].str.contains(SEARCH_WORD.lower(), flags=re.I)]
     # sorted_file = file
     file = file['Count']  # данные
     for row in file:
         str3.append(str(row))
-    str3 = sorted(set(str3))
+    str4 = sorted(set(str3))
 
-    return str3  # , sorted_file
+    return str4  # , sorted_file
 
 
 def saver(str1, df):
     list_of_sets = []
     for elem in str1:
-        counts = count_finder(df, elem,'Data')
+        counts = count_finder(df, elem, 'Data')
         list_of_sets.append(counts)
 
     df2 = pd.DataFrame({'Data': str1, 'List of sets': list_of_sets})
@@ -227,7 +195,7 @@ file = pd.read_csv(FILE, encoding='PT154')
 
 # удаляет полные дубли по 2м столбцам
 file_1 = file.drop_duplicates(subset=None, keep='first')
-
+# print(file_1)
 # Cчитываем столбцы
 str1, str2 = reader(file_1)
 # print("Str1:",str1[:2])# str1 ['cущ1 сущ2 сущ3','сущ1 сущ2 сущ3',...]
@@ -242,19 +210,192 @@ file_2 = file_2.drop_duplicates(subset=None, keep='first')
 # Формируем новый файл file_3 {'Set of counts':[10.08, 10.09], 'TEG':'бита'}
 file_3, set_bad_tegs = teg_to_counts(str1=str1, df=file_2)
 # print(file_3)
-# print(set_bad_tegs)
+#print(set_bad_tegs)
 
 # Сохраним наработки по file3
 pure_saver(df=file_3, name_of_file='data/df_teg_to_counts.csv')
 
 # Нашли все строки содержащие и не содержащие set_bad_tegs
 list_of_bad_str, list_of_good_str = str_finder(file_1, set_bad_tegs)
-# print(list_of_bad_str)
+
+# Создадим [['Count','Data'],['Count','Data'],[]] для good c исходными значениями
+df = file_1.values
+good_elems = []
+for elem in df:
+    for i in elem:
+        if i in list_of_good_str:
+            elem = list(elem)
+            good_elems.append(elem)
+            # print(elem)
+
+
+# print(good_elems)#[['10.01', '90 минут 2 поездка  17.12.14 МосГосТранс'], ['10.12', 'Средство чистящее универсальное
+
+
+def reclass_bad_str(set_bad_tegs, list_of_bad_str, str2):
+    cc = list()
+    bbbb = list()
+    qqqq = list()
+    jj = list()
+    print(set(str2))
+    for word in set_bad_tegs:  # бита
+        bb = list()
+        for elem in list_of_bad_str:
+            elem_low = elem.lower()
+            if (elem_low.find(word) != -1):
+                bb.append(elem)
+        # print(word,bb)#бита ['БИТА',...]
+
+        print('Введите номер счета следующей группе строк с TEGом -', word, '\n')
+        for i in bb:
+            print(' ', i)
+        input_count = input('\nВведите номер счета:')
+        print('Вы ввели', input_count, '\n')
+        input_count = str(input_count)
+
+
+        if input_count in set(str2):
+            cc.append(input_count)
+        else:
+            input_count = 'UNK'
+            cc.append(input_count)
+        bbbb.append(bb)
+
+        for i in bb:
+            qq = [input_count]
+            qq.append(i)
+            qqqq.append(qq)
+    # print(cc)
+    # jj=list(zip(cc, bbbb))
+    # print(list(zip(cc,bbbb)))#[('12', ['БИТА', 'Бита', 'Бита 1/4"', 'Бита 100мм', 'Бита ЗУБР МАСТЕР с 1/4 НР 2']),
+    print(type(qqqq))
+
+    #удалим дубли в qqqq
+
+    return qqqq
+
+
+# нужно доработать qqqq на наличие одинаковых элементов, которые были инициализированы по разному
+
+
+# vocab, vocab_1=reclass_bad_str(set_bad_tegs)
+qqqq = reclass_bad_str(set_bad_tegs, list_of_bad_str,str2)
+
+# print(vocab)
+
+
+ultra = qqqq + good_elems
+
+
+for i in ultra:
+    print(' ', i)
+
+print(type(ultra))
+print(len(ultra))
+
+'''
+1)
+если аналитик не знает как классифицировать, 
+или код не удовлетворяет формату счета из словаря счетов, 
+ставь UNK
+
+2)
+preprocessor
+исследовать три первых слова для тега 
+
+3)
+Расстояние левинштейна, 
+доп проверка, были ли использованы в классификации следующие строки
+
+'''
+
+# Rubbish
+'''
+def reclass_bad_str(set_bad_tegs,list_of_bad_str):
+    cc=list()
+    bbbb=list()
+    qqqq=list()
+    jj=list()
+    for word in set_bad_tegs:#бита
+        bb=list()
+        for elem in list_of_bad_str:
+            elem_low=elem.lower()
+            if (elem_low.find(word) != -1):
+                bb.append(elem)
+        #print(word,bb)#бита ['БИТА',...]
+
+
+        print('Введите номер счета следующей группе строк с TEGом -', elem, '\n')
+        for i in bb:
+            print(' ', i)
+        input_count = input('\nВведите номер счета:')
+        print('Вы ввели', input_count, '\n')
+        input_count=str(input_count)
+        cc.append(input_count)
+        bbbb.append(bb)
+
+        for i in bb:
+            qq=[input_count]
+            qq.append(i)
+            qqqq.append(qq)
+    #print(cc)
+    #jj=list(zip(cc, bbbb))
+    #print(list(zip(cc,bbbb)))#[('12', ['БИТА', 'Бита', 'Бита 1/4"', 'Бита 100мм', 'Бита ЗУБР МАСТЕР с 1/4 НР 2']),
+    #print(qqqq)
+    return qqqq
+
+
+buffer = list()
+        for i in list_of_bad_str:
+            i_low = i.lower()
+            if (i_low.find(word) != -1):
+                buffer.append(i)
+            vocab[word] = buffer
+            
+    #vocab_1 = vocab  # слово и все строки исходного файла содержащие bad word #х
+
+   
+    for elem in vocab:
+        bb = []
+        print('Введите номер счета следующей группе строк с TEGом -', elem, '\n')
+        for i in vocab[elem]:
+            print(' ', i)
+        input_count = input('\nВведите номер счета:')
+        print('Вы ввели', input_count, '\n')
+        input_count = str(input_count)
+        vocab[input_count] = vocab.pop(elem)
+
+        bb.append(input_count)
+        bb.append(vocab[input_count])
+        bbbb.append(bb)
+    #print(bbbb)
+    return vocab, vocab_1
+
+
+
+
+
+
+
+
+
+df = file_1.values
+good_elems = []
+for elem in df:
+    for i in elem:
+        if i in list_of_good_str:
+            elem = list(elem)
+            good_elems.append(elem)
+            #print(elem)
+
+
+
 # создадим словарь слово:строки где встречаются
 
-def bb ():
-    vocab, vocab_1 = dict()
-    buffer = set()
+def reclass_bad_str(set_bad_tegs):
+    vocab = dict()
+
+    bbbb=[]
 
     for word in set_bad_tegs:
         buffer = set()
@@ -262,43 +403,96 @@ def bb ():
             i_low = i.lower()
             if (i_low.find(word) != -1):
                 buffer.add(i)
-                # print(buffer)
             vocab[word] = buffer
-    vocab_1=vocab
-    #print(tabulate(vocab.items(), headers=['word', 'strings'], tablefmt="grid"))
+    vocab_1 = vocab#слово и все строки исходного файла содержащие bad word
+
 
     for elem in vocab:
-        print('Введите номер счета следующей группе строк с TEGом -',elem,'\n')
-        #print(vocab[elem])
+        bb = []
+        print('Введите номер счета следующей группе строк с TEGом -', elem, '\n')
         for i in vocab[elem]:
-            print(' ',i)
+            print(' ', i)
         input_count = input('\nВведите номер счета:')
-        print('Вы ввели',input_count,'\n')
-        input_count=str(input_count)
+        print('Вы ввели', input_count, '\n')
+        input_count = str(input_count)
         vocab[input_count] = vocab.pop(elem)
-    print(vocab)
-    return vocab
 
-str3=count_finder(file=file_1, SEARCH_WORD='Бита ЗУБР МАСТЕР с 1/4 НР 2',column_name='Fullname')
-print(str3)
-
-
-
-
-'''
-нужно допилить функицию которая бы по bad_words
-находила строки содержащие эти слова в исходном файле 
-и из них создала бы файл состоящий из неправильно классифицированных строк
-плюс нужно учесть, что лучшим решением стало бы записывать в файл по словам  
-'''
+        bb.append(input_count)
+        bb.append(elem)
+    bbbb.append(bb)
+    print(bbbb)
+    return vocab, vocab_1
 
 
+vocab, vocab_1 = reclass_bad_str(set_bad_tegs)
+#new_dict = list(zip(vocab_count, vocab_value))
+
+#print(vocab)#{'12': {'Бита 100мм', 'БИТА', 'Бита 1/4"', 'Бита', 'Бита ЗУБР МАСТЕР с 1/4 НР 2'}, '13': {'Стол “Премьер-Элит“ разм. 80Х150х120см черн.', 'Костюм охрана-с размер 56-58  рост 182-188   ( депо Баумана)', 'Перчатки желтые резиновые размер XL '}, '14': {'Костюм охрана-с размер 56-58  рост 182-188   ( депо Баумана)', 'Перчатки желтые резиновые размер XL '}}
+#vocab={'12': {'Бита 100мм', 'БИТА', 'Бита 1/4"', 'Бита', 'Бита ЗУБР МАСТЕР с 1/4 НР 2'}, '13': {'Стол “Премьер-Элит“ разм. 80Х150х120см черн.', 'Костюм охрана-с размер 56-58  рост 182-188   ( депо Баумана)', 'Перчатки желтые резиновые размер XL '}, '14': {'Костюм охрана-с размер 56-58  рост 182-188   ( депо Баумана)', 'Перчатки желтые резиновые размер XL '}}
+
+
+good_elems=[]
+bad_elems=[]
+df = file_1.values
+for elem in df:
+    for i in elem:
+        if i in list_of_good_str:
+            good_elems.append(elem)
+        else:
+            bad_elems.append(elem)
+#print(good_elems)
 
 
 
 
-# Rubbish
-'''
+
+for elem in bb:
+    for i in elem:
+        if (i%2==0):
+            for j in i:
+
+
+print(bb)
+
+# создать словарь с правильными словами (все - принадлежащие к неправильным)
+
+file_str = file_1['Fullname']
+vocab_value = []
+for row in file_str:
+    vocab_value.append(row)
+#print(vocab_value)
+
+file_count = file_1['Count']
+vocab_count = []
+for row in file_count:
+  vocab_count.append(row)
+#print(vocab_count)
+
+new_dict = list(zip(vocab_count, vocab_value))
+for elem in new_dict:
+    for i in elem:
+        if (i==)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(len(vocab_of_all))
+print(tabulate(vocab_of_all.items(), headers=['Count', 'Strings'], tablefmt="grid"))
+
+
+
 def bad_str_finder(set_bad_words,file):  # на вход nomenklatura
 
     list_of_str = []
@@ -988,4 +1182,3 @@ def preprocessor(data_file):
     return words
 
 '''
-
