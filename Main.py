f547f28756feaa1f006c3893802d5b4dce1b66ad
Stanislav <34125332+StanislavKoval@users.noreply.github.com>
2019-04-26 01:39:47 +0300

    Update Main.py
    финальная v.1.0.1

--- Main.py
+++ Main.py
@@ -139,8 +139,10 @@ def teg_to_counts(str1, df):
     one_str, set_of_counts = [], []
     set_of_tegs = set()
     for elem in str1:
-        one_str = elem.split()
-        for word in one_str:
+        one_str = elem.split()  # list [word0, word1, word2, ...]
+        # для словаря тегов используем только первые 3 слова строки
+
+        for word in one_str[:2]:#возьмем для словаря тегов только первые 2 слова
             set_of_tegs.add(word)  # set тегов
     set_of_tegs = sorted(set_of_tegs)  # отсортированный по алфавиту set тегов без повтора
 
@@ -186,131 +188,183 @@ def str_finder(file, set_bad_words):  # на вход nomenklatura
     return list_of_bad_str, list_of_good_str
 
 
+def good_finder(file, list_of_good_str):
+    df = file.values
+    good_elems = []
+    for elem in df:
+        for i in elem:
+            if i in list_of_good_str:
+                elem = list(elem)
+                good_elems.append(elem)
+                # print(elem)
+
+    # [['10.01', '90 минут 2 поездка  17.12.14 МосГосТранс'], ['10.12', 'Средство чистящее универсальное
+    # print(good_elems)
+    return good_elems
+
+
+def reclass_bad_str(set_bad_tegs, list_of_bad_str, list_of_counts):
+    list_of_new_counts = list()
+    bbbb = list()
+    bad_elems = list()
+    # print(set(str2))
+
+    # для каждого слова из set_bad_tegs, найдем те строки, в которых он был зафиксирован
+    for word in set_bad_tegs:
+
+        word_bad_str = list()
+        for elem in list_of_bad_str:
+            elem_low = elem.lower()
+            if (elem_low.find(word) != -1):
+                word_bad_str.append(elem)
+                # ['Bad_str_0','Bad_str_1',...]
+
+        # Ручной ввод
+        print('Введите номер счета следующей группе строк с TEGом -', word, '\n')
+        for i in word_bad_str:
+            print(' ', i)
+        input_count = input('\nВведите номер счета:')
+        print('Вы ввели', input_count, '\n')
+        input_count = str(input_count)
+
+        # Условие для UNK
+        if input_count in set(list_of_counts):
+            list_of_new_counts.append(input_count)
+        else:
+            input_count = 'UNK'
+            list_of_new_counts.append(input_count)
+        #bbbb.append(word_bad_str)
+
+        # bad_elems = [('input_count', ['БИТА', 'Бита', 'Бита 1/4"', 'Бита 100мм', 'Бита ЗУБР МАСТЕР с 1/4 НР 2']),
+        for i in word_bad_str:
+            one_elem = [input_count]
+            one_elem.append(i)
+            bad_elems.append(one_elem)
+
+    return bad_elems
+#в случае если bad word содержится только в 1 предложении, сохранять старый
+
 #########################################################################################################
 FILE = 'data/123.csv'
 # FILE = 'data/nomenklatura.csv'
 
-# считываем файл
+# Считываем файл
 file = pd.read_csv(FILE, encoding='PT154')
 
-# удаляет полные дубли по 2м столбцам
+# Удаляет полные дубли по 2м столбцам
 file_1 = file.drop_duplicates(subset=None, keep='first')
 # print(file_1)
-# Cчитываем столбцы
+
+# Считываем столбцы
 str1, str2 = reader(file_1)
 # print("Str1:",str1[:2])# str1 ['cущ1 сущ2 сущ3','сущ1 сущ2 сущ3',...]
 # print("Str2:",str2[:2])# str2 [Count1,Count2,...]
 
 # Формируем новый файл file_2 {'Count': str2, 'Data': str1}
 file_2 = pd.DataFrame({'Count': str2, 'Data': str1})
-# удаляет полные дубли по 2м столбцам
+
+# Удаляет полные дубли по 2м столбцам
 file_2 = file_2.drop_duplicates(subset=None, keep='first')
 # print(file_2)
 
 # Формируем новый файл file_3 {'Set of counts':[10.08, 10.09], 'TEG':'бита'}
 file_3, set_bad_tegs = teg_to_counts(str1=str1, df=file_2)
 # print(file_3)
-#print(set_bad_tegs)
+# print(set_bad_tegs)
 
-# Сохраним наработки по file3
-pure_saver(df=file_3, name_of_file='data/df_teg_to_counts.csv')
+# Сохраним наработки по file_3
+pure_saver(df=file_3, name_of_file='data/df_teg_to_counts12.csv')
 
 # Нашли все строки содержащие и не содержащие set_bad_tegs
-list_of_bad_str, list_of_good_str = str_finder(file_1, set_bad_tegs)
+list_of_bad_str, list_of_good_str = str_finder(file=file_1, set_bad_words=set_bad_tegs)
 
 # Создадим [['Count','Data'],['Count','Data'],[]] для good c исходными значениями
-df = file_1.values
-good_elems = []
-for elem in df:
-    for i in elem:
-        if i in list_of_good_str:
-            elem = list(elem)
-            good_elems.append(elem)
-            # print(elem)
+good_elems = good_finder(file=file_1, list_of_good_str=list_of_good_str)
+# [['10.01', '90 минут 2 поездка  17.12.14 МосГосТранс'], ['10.12', 'Средство чистящее универсальное
+
+# Создадим [['Count','Data'],['Count','Data'],[]] для bad cо значениями введенными с клавиатуры
+bad_elems = reclass_bad_str(set_bad_tegs=set_bad_tegs, list_of_bad_str=list_of_bad_str, list_of_counts=str2)
+# [['10.10', 'Бита ЗУБР МАСТЕР с 1/4 НР 2'],['UNK', 'Костюм охрана-с размер 56-58  рост 182-188   ( депо Баумана)']
+
+# Cложим 2 получившихся списков, для восстановления обновленного строя данных
+new_data_list = bad_elems + good_elems
+# new_data_list=[['10.10', 'БИТА'], ['10.10', 'Бита'], ['10.10', 'Бита 1/4"'], ['10.10', 'Бита 100мм'], ['10.10', 'Бита ЗУБР МАСТЕР с 1/4 НР 2'], ['UNK', 'Костюм охрана-с размер 56-58  рост 182-188   ( депо Баумана)'], ['UNK', 'Перчатки желтые резиновые размер XL '], ['10.01', '90 минут 2 поездка  17.12.14 МосГосТранс'], ['10.12', 'Средство чистящее универсальное (Пемолюкс 480 г)'], ['10.113', 'Принтер сканер Brother XXXX'], ['10.12', 'Стол “Премьер-Элит“ разм. 80Х150х120см черн.'], ['10.11.18', 'Путевка „"Golden CARD"“ пансионат «"Ананасовый изумруд"»  "ВАО Москва"'], ['10.12', 'деталь шайба завод «Ракетно космическая корпорация»'], ['10.05', 'Утяжелитель конуса КС-27'], ['10.05', 'Ф/диск KingSton DT101G2/32GB'], ['10.05', 'Фара (блок фары) 30.3775.000 правая МАЗ Евро'], ['10.05', 'Фара (блок фары) левая МАЗ Евро'], ['10.05', 'Фара (фонарь)противотуман.'], ['10.05', 'Фара б/у'], ['10.05', 'Фара в сборе'], ['10.05', 'Фара Волжанин ближнего света'], ['10.05', 'Фара Волжанин'], ['10.05', 'Фара Г-3102'], ['10.09', 'Билет входной'], ['10.09', 'Биотуалет']]
 
 
-# print(good_elems)#[['10.01', '90 минут 2 поездка  17.12.14 МосГосТранс'], ['10.12', 'Средство чистящее универсальное
+# Преобразуем вложенный список в 2 списка: список счетов, спиок обновленный данных
+new_data_list = np.array(new_data_list)
+new_list_of_counts = new_data_list[:, [0]].ravel()
+new_list_of_fullname = new_data_list[:, [1]].ravel()
+
+# Формируем новый файл file_4
+file_4 = pd.DataFrame({'Count': new_list_of_counts, 'Fullname': new_list_of_fullname})
+
+# Удаляет полные дубли по 2м столбцам
+file_4 = file_4.drop_duplicates(subset=None, keep='first')
+
+# Можно отсортировать по алфавиту
+file_4 = file_4.sort_values(['Fullname', 'Count'], ascending=[True, False])
+print(file_4)
+
+# Сохраним наработки по file_4
+pure_saver(df=file_4, name_of_file='data/final_for_test.csv')
 
 
-def reclass_bad_str(set_bad_tegs, list_of_bad_str, str2):
-    cc = list()
-    bbbb = list()
-    qqqq = list()
-    jj = list()
-    print(set(str2))
-    for word in set_bad_tegs:  # бита
-        bb = list()
-        for elem in list_of_bad_str:
-            elem_low = elem.lower()
-            if (elem_low.find(word) != -1):
-                bb.append(elem)
-        # print(word,bb)#бита ['БИТА',...]
-
-        print('Введите номер счета следующей группе строк с TEGом -', word, '\n')
-        for i in bb:
-            print(' ', i)
-        input_count = input('\nВведите номер счета:')
-        print('Вы ввели', input_count, '\n')
-        input_count = str(input_count)
 
 
-        if input_count in set(str2):
-            cc.append(input_count)
-        else:
-            input_count = 'UNK'
-            cc.append(input_count)
-        bbbb.append(bb)
-
-        for i in bb:
-            qq = [input_count]
-            qq.append(i)
-            qqqq.append(qq)
-    # print(cc)
-    # jj=list(zip(cc, bbbb))
-    # print(list(zip(cc,bbbb)))#[('12', ['БИТА', 'Бита', 'Бита 1/4"', 'Бита 100мм', 'Бита ЗУБР МАСТЕР с 1/4 НР 2']),
-    print(type(qqqq))
-
-    #удалим дубли в qqqq
-
-    return qqqq
 
 
-# нужно доработать qqqq на наличие одинаковых элементов, которые были инициализированы по разному
 
-
-# vocab, vocab_1=reclass_bad_str(set_bad_tegs)
-qqqq = reclass_bad_str(set_bad_tegs, list_of_bad_str,str2)
-
-# print(vocab)
-
-
-ultra = qqqq + good_elems
-
-
-for i in ultra:
-    print(' ', i)
-
-print(type(ultra))
-print(len(ultra))
 
 '''
-1)
+1)+
 если аналитик не знает как классифицировать, 
 или код не удовлетворяет формату счета из словаря счетов, 
 ставь UNK
 
-2)
+2)+
+    Выделять для тегов только первые 3 слова в предложениее
 preprocessor
 исследовать три первых слова для тега 
 
 3)
+    Проверить теги на близость друг другу
 Расстояние левинштейна, 
 доп проверка, были ли использованы в классификации следующие строки
+
+4)
+удалить дубли в qqqq
+
+5)+
+вывести все в файл и сохранить файл
+
+6)+
+оптимизировать и причесать код
+
+
+
+прогон малой части
+подготовить доклад о проделанной работе за полгода 
+
+препроцессор для номенклатурных данных
+word2vec Тани
+переписал проект first
+
+тестирование МИА
+Теория по НС, курс козлова, книга
+Разметка данных
+
 
 '''
 
 # Rubbish
 '''
+    # print(cc)
+    # jj=list(zip(cc, bbbb))
+    # print(list(zip(cc,bbbb)))
+
+
+
 def reclass_bad_str(set_bad_tegs,list_of_bad_str):
     cc=list()
     bbbb=list()
@@ -1182,3 +1236,4 @@ def preprocessor(data_file):
     return words
 
 '''
+
